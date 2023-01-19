1aeabc75eff791edd9373d88c534d4d23497ea51
nuwanuom18 <nuwan.18@cse.mrt.ac.lk>
2023-01-19 21:58:45 +0530

    remove some unused files

--- test.sh
+++
@@ -1,7 +0,0 @@
-status="NOT COMPLETED"
-while [ ! -z "$status" ]
-do
-    status=$(gh pr status --jq '.currentBranch .statusCheckRollup[] | select(.status!="COMPLETED")' --json statusCheckRollup)
-    sleep 2
-done
-echo "finished now"
